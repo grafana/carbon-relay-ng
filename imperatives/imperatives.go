9fde48179477dde57cc8d67f9ffad8cb6fcb95db
Dieter Plaetinck <dieter@grafana.com>
2018-01-26 13:59:22 +0100

    default grafanaNet to 10s
    for most of our customers this makes more sense

--- imperatives/imperatives.go
+++ imperatives/imperatives.go
@@ -406,7 +406,7 @@ func readAddRouteGrafanaNet(s *toki.Scanner, table Table) error {
 	var bufSize = int(1e7)  // since a message is typically around 100B this is 1GB
 	var flushMaxNum = 10000 // number of metrics
 	var flushMaxWait = 500  // in ms
-	var timeout = 5000      // in ms
+	var timeout = 10000     // in ms
 	var concurrency = 10    // number of concurrent connections to tsdb-gw
 	var orgId = 1
 
