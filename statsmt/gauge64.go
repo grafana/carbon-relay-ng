0558bb0a8ea8bd171832805cf39610e724438d3b
Dieter Plaetinck <dieter@grafana.com>
2019-04-29 23:30:26 +0300

    add metrictank's stats package

---
+++ statsmt/gauge64.go
@@ -0,0 +1,59 @@
+package statsmt
+
+import (
+	"sync/atomic"
+	"time"
+)
+
+type Gauge64 uint64
+
+func NewGauge64(name string) *Gauge64 {
+	u := Gauge64(0)
+	return registry.getOrAdd(name, &u).(*Gauge64)
+}
+
+func (g *Gauge64) Inc() {
+	atomic.AddUint64((*uint64)(g), 1)
+}
+
+func (g *Gauge64) Dec() {
+	atomic.AddUint64((*uint64)(g), ^uint64(0))
+}
+
+func (g *Gauge64) AddUint64(val uint64) {
+	atomic.AddUint64((*uint64)(g), val)
+}
+
+func (g *Gauge64) DecUint64(val uint64) {
+	atomic.AddUint64((*uint64)(g), ^uint64(val-1))
+}
+
+func (g *Gauge64) Add(val int) {
+	if val == 0 {
+		return
+	}
+	if val > 0 {
+		g.AddUint64(uint64(val))
+		return
+	}
+	// < 0
+	g.DecUint64(uint64(-1 * val))
+}
+
+func (g *Gauge64) Set(val int) {
+	atomic.StoreUint64((*uint64)(g), uint64(val))
+}
+
+func (g *Gauge64) SetUint64(val uint64) {
+	atomic.StoreUint64((*uint64)(g), val)
+}
+
+func (g *Gauge64) ReportGraphite(prefix, buf []byte, now time.Time) []byte {
+	val := atomic.LoadUint64((*uint64)(g))
+	buf = WriteUint64(buf, prefix, []byte("gauge64"), val, now)
+	return buf
+}
+
+func (g *Gauge64) Peek() uint64 {
+	return atomic.LoadUint64((*uint64)(g))
+}
---
+++ statsmt/latencyhistogram15s32.go
@@ -0,0 +1,49 @@
+package statsmt
+
+import (
+	"sync/atomic"
+	"time"
+
+	"github.com/Dieterbe/artisanalhistogram/hist15s"
+)
+
+// tracks latency measurements in a given range as 32 bit counters
+type LatencyHistogram15s32 struct {
+	hist  hist15s.Hist15s
+	since time.Time
+	sum   uint64 // in micros. to generate more accurate mean
+}
+
+func NewLatencyHistogram15s32(name string) *LatencyHistogram15s32 {
+	return registry.getOrAdd(name, &LatencyHistogram15s32{
+		hist:  hist15s.New(),
+		since: time.Now(),
+	},
+	).(*LatencyHistogram15s32)
+}
+
+func (l *LatencyHistogram15s32) Value(t time.Duration) {
+	atomic.AddUint64(&l.sum, uint64(t.Nanoseconds()/1000))
+	l.hist.AddDuration(t)
+}
+
+func (l *LatencyHistogram15s32) ReportGraphite(prefix, buf []byte, now time.Time) []byte {
+	snap := l.hist.Snapshot()
+	// TODO: once we can actually do cool stuff (e.g. visualize) histogram bucket data, report it
+	// for now, only report the summaries :(
+	r, ok := l.hist.Report(snap)
+	if ok {
+		sum := atomic.SwapUint64(&l.sum, 0)
+		buf = WriteUint32(buf, prefix, []byte("latency.min.gauge32"), r.Min/1000, now)
+		buf = WriteUint32(buf, prefix, []byte("latency.mean.gauge32"), uint32((sum / uint64(r.Count) / 1000)), now)
+		buf = WriteUint32(buf, prefix, []byte("latency.median.gauge32"), r.Median/1000, now)
+		buf = WriteUint32(buf, prefix, []byte("latency.p75.gauge32"), r.P75/1000, now)
+		buf = WriteUint32(buf, prefix, []byte("latency.p90.gauge32"), r.P90/1000, now)
+		buf = WriteUint32(buf, prefix, []byte("latency.max.gauge32"), r.Max/1000, now)
+	}
+	buf = WriteUint32(buf, prefix, []byte("values.count32"), r.Count, now)
+	buf = WriteFloat64(buf, prefix, []byte("values.rate32"), float64(r.Count)/now.Sub(l.since).Seconds(), now)
+
+	l.since = now
+	return buf
+}
