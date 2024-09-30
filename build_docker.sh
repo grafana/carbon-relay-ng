0dfdcc7c66b526b9553498f9019c803bd3f036e2
Nicolás Pazos <npazosmendez@gmail.com>
2024-09-30 18:21:23 -0300

    fix: correctly check whether CI is running on release or not

--- build_docker.sh
+++ build_docker.sh
@@ -5,7 +5,7 @@ version=$(git describe --tags --always | sed 's/^v//')
 # note: we may want to extend this to also not tag as latest if working tree is dirty.
 # but i think because of how go bindata works, it probably makes a change in the working tree.
 tag=master
-[[ "$GITHUB_EVENT_NAME" = "push" ]] && [[ "$GITHUB_REF_TYPE" = "tag" ]] && [[ "$version" != *-* ]] && tag=latest
+[[ "$GITHUB_EVENT_NAME" = "push" ]] && [[ "$GITHUB_REF_TYPE" = "tag" ]] && [[ "$version" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]] && tag=latest
 
 
 docker build --tag=grafana/carbon-relay-ng:$tag .
